3e16f1d2305459dda0f01250b2a89170e349f901
DanielMehlber <daniel.mehlber@gmail.com>
2021-04-20 20:11:40 +0200

    removed device check. was dumb.

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,7 +5,7 @@ echo "Installing mate-touch-rotator to /opt/mate-touch-rotate/"
 echo "Checking input devices..."
 devices=$(xinput --list)
 # check if touchscreen name ist defined
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "Error: Please pass the touchscreen's name as an argument"
     echo "Like this: INSTALL.sh [TOUCHSCREEN_NAME] [TOUCHPAD_NAME]"
     echo "Devices are: (according to 'xinput --list')"
@@ -14,17 +14,8 @@ if [ -z $1 ]; then
     return
 fi
 
-# check if touchscreen device exists
-if [[ $devices != *"$1"* && "$1" != "None" ]]; then
-    echo "Error: $1 is not a valid device"
-    echo "Devices are: (according to 'xinput --list')"
-    echo "$devices"
-    echo "Or just 'None'"
-    return 
-fi
-
 # check if touchpad name is defined
-if [ -z $2 ]; then
+if [ -z "$2" ]; then
     echo "Error: Please also pass the touchpad's name as an argument"
     echo "Like this: INSTALL.sh $1 [TOUCHPAD_NAME]"
     echo "Devices are: (according to 'xinput --list')"
@@ -33,14 +24,6 @@ if [ -z $2 ]; then
     return
 fi
 
-#check if touchpad device exists
-if [[ $devices != *"$2"* && "$2" != "None" ]]; then
-    echo "Error: $2 is not a valid device"
-    echo "Devices are: (according to 'xinput --list')"
-    echo "$devices"
-    echo "Or just 'None'"
-    return
-fi
 
 sudo mkdir "/opt/mate-touch-rotate"
 # move script file to installation directory
